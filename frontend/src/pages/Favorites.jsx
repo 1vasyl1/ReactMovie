3c988e7c5e012b501f8f7beb37dd65bedb3069f2
1vasyl1 <vasylhrytsyshyn1@gmail.com>
2025-05-14 22:12:28 +0200

    Initial commit

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import '../css/Favorites.css'
+
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favrotie Movies Yet</h2>
+        <p>Start adding movies ot your favorites and they willer apper her</p>
+    </div>
+}
+
+export default Favorites;
